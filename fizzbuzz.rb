d3a5fa07a4eeb4d42cea72df3bb340e5f1c5c54b
Menachem Mendel Konikov <mkonikov123@gmail.com>
2020-09-24 20:55:14 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,10 @@
 def fizzbuzz(int)
-  if int % 3 == 0 do
+  if int % 3 == 0
     "Fizz"
-  elsif int % 5 == 0 do
+  elsif int % 5 == 0
     "Buzz"
-  elsif int % 3 && 5 do
+  elsif int % 3 || 5
     "FizzBuzz"
+  else
+    "nil"
   end
